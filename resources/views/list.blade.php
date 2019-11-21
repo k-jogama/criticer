c4267f1bd97dda73b29cb0f433f93d0c33f9677b
jogama <pg.kj50@gmail.com>
2019-11-21 23:08:22 +0900

    list view 追加

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -4,45 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8 eatlogurl-area">
-            <div class="eatlogdata-list-btn-area">
-                <a href="{{ route('list') }}">
-                    <button type="button" class="">★お気に入り</button>
-                </a>
-            </div>
-            <form action="{{ route('home') }}" method="POST" class="form-url">
-                {{ csrf_field() }}
-                <div><input type="url" name="eatlogurl" class="eatlogurl" placeholder="食べログのURLを入力してください"></div>
-                <div><button type="submit" class="btn-send">送信</button></div>
-            </form>
-
-            @if(isset($eatlogdata))
-                <form action="{{ route('save') }}" method="POST" class="form-save">
-                    {{ csrf_field() }}
-                    <div class="eatlogdata-display-area">
-                        <table class="table table-striped table-bordered">
-                            <tbody>
-                                @foreach($eatlogdata as $key => $item)
-                                    @if($key != 'img')
-                                    <tr>
-                                        <th>{{ $item['label'] }}</th>
-                                        @if($key == 'eatlogurl' || $key == 'hp')
-                                            <td><a href="{{ $item['info'] }}" target="_blank">{{ $item['info'] }}</a></td>
-                                        @else
-                                            <td>{{ $item['info'] }}</td>
-                                        @endif
-                                    </tr>
-                                    @endif
-                                    <input type="hidden" name="{{ $key }}" value="{{ $item['info'] }}">
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div class="eatlogdata-save-btn-area">
-                        <button type="submit" class="btn-save">★お気に入り登録</button>
-                    </div>
-                </form>
-            @endif
+            <table class="table table-striped table-bordered">
+                <tbody>
+                <tr>
+                    <th>店舗名</th>
+                    <td>ああああ</td>
+                </tr>
+                <tr>
+                    <th>評価</th>
+                    <td>3.7</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
