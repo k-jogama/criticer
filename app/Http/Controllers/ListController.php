5440c9a1b3cd673fafbd966e38153e95a30add0e
Kento Jogama <jyougama@groon.co.jp>
2019-11-22 17:23:06 +0900

    list ロジック修正

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ListController extends Controller
 {
     public function index()
     {
-        return view('list');
+        $eatlogs = DB::table('eatlogs')->orderBy('id', 'desc')->get();
+        return view('list', compact('eatlogs'));
     }
 }
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,21 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 eatlogurl-area">
-            <table class="table table-striped table-bordered">
-                <tbody>
-                <tr>
-                    <th>店舗名</th>
-                    <td>ああああ</td>
-                </tr>
-                <tr>
-                    <th>評価</th>
-                    <td>3.7</td>
-                </tr>
-                </tbody>
-            </table>
+<div id="eatlog-list">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 eatlogurl-area">
+                <div class="eatlogdata-list-area">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="item-s">{{ config('const.EATLOGS.NAME_LIST.Shopname') }}</th>
+                                <th class="item-s">{{ config('const.SCORE') }}</th>
+                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Reserve_tel') }}</th>
+                                <th class="item-s">{{ config('const.EATLOGS.NAME_LIST.Reserve_judgment') }}</th>
+                                <th class="item-l">{{ config('const.EATLOGS.NAME_LIST.Address') }}</th>
+                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Business_hours') }}</th>
+                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Shoptel') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($eatlogs as $item)
+                        <tr>
+                            <td>{{ $item->shopname }}</td>
+                            <td>{{ $item->score }}</td>
+                            <td>{{ $item->reserve_tel }}</td>
+                            <td>{{ $item->reserve_judgment }}</td>
+                            <td>{{ $item->address }}</td>
+                            <td>{{ $item->business_hours }}</td>
+                            <td>{{ $item->shoptel }}</td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
