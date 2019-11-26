36af9cff39782c1b553770b6697b8aea03026bb0
Kento Jogama <jyougama@groon.co.jp>
2019-11-26 18:45:17 +0900

    アラート表示

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,34 @@
 (function() {
     'use strict';
 
-    // フラッシュメッセージのfadeout
     $(function(){
-        $('.flash_message').fadeOut(3000);
+        $('.msg_success').fadeOut(3000);
+    });
+
+    $(function(){
+        $('.msg_danger').fadeOut(3000);
+    });
+
+    $('.btn-send').on('click', function() {
+        if (!$('.eatlogurl').val()) {
+            alert('URLを入力してください');
+            return false;
+        }
+        $('.form-url').submit();
+    });
+
+    $('.eatlog-save').on('click', function () {
+        if (!$('.shopname').val()) {
+            alert('店舗名を入力してください');
+            return false;
+        }
+        $('.edit-form').submit();
+    });
+
+    $('.eatlog-delete').on('click', function () {
+        if (!confirm('削除してよろしいですか？')) {
+            return false;
+        }
     });
 
 })();
