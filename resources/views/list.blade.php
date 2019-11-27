c556df5ae7ab293aadd313817b5f0fd773d4924b
Kento Jogama <jyougama@groon.co.jp>
2019-11-27 18:29:44 +0900

    list 表示項目修正

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -12,22 +12,24 @@
                                 <th class="item-s">{{ config('const.EATLOGS.NAME_LIST.Shopname') }}</th>
                                 <th class="item-s">{{ config('const.SCORE') }}</th>
                                 <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Reserve_tel') }}</th>
-                                <th class="item-s">{{ config('const.EATLOGS.NAME_LIST.Reserve_judgment') }}</th>
                                 <th class="item-l">{{ config('const.EATLOGS.NAME_LIST.Address') }}</th>
                                 <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Business_hours') }}</th>
                                 <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Shoptel') }}</th>
+                                <th class="item-s">{{ config('const.EDIT') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($eatlogs as $item)
-                            <tr>
-                                <td><a href="{{ action('EditController@edit', $item->id) }}">{{ $item->shopname }}</a></td>
+                                <td>
+                                    <div><a href="{{ action('DetailController@show', $item->id) }}">{{ $item->shopname }}</a></div>
+                                    <div><a href="{{ $item->eatlogurl }}" target="_blank"><button type="button" class="eatlog-btn">{{ config('const.EATLOG') }}</button></a></div>
+                                </td>
                                 <td>{{ $item->score }}</td>
                                 <td><a href="tel:{{ $item->reserve_tel }}">{{ $item->reserve_tel }}</a></td>
-                                <td>{{ $item->reserve_judgment }}</td>
                                 <td><a href="https://maps.google.co.jp/maps/search/{{ $item->address }}" target="_blank">{{ $item->address }}</a></td>
                                 <td>{{ $item->business_hours }}</td>
                                 <td><a href="tel:{{ $item->shoptel }}">{{ $item->shoptel }}</a></td>
+                                <td><a href="{{ action('EditController@edit', $item->id) }}"><button type="button" class="edit-btn">{{ config('const.EDIT') }}</button></a></td>
                             </tr>
                             @endforeach
                         </tbody>
