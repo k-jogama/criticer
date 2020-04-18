c7063657b1195a38d5d27fc4e3c2ef5f2e2d1bde
k-jogama <pg.kj50@gmail.com>
2020-04-18 12:57:25 +0900

    一覧画面レイアウト修正

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,42 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="eatlog-list">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12 eatlogurl-area">
-                <div class="eatlogdata-list-area">
-                    <table class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th class="item-s">{{ config('const.EATLOGS.NAME_LIST.Shopname') }}</th>
-                                <th class="item-s">{{ config('const.SCORE') }}</th>
-                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Reserve_tel') }}</th>
-                                <th class="item-l">{{ config('const.EATLOGS.NAME_LIST.Address') }}</th>
-                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Business_hours') }}</th>
-                                <th class="item-m">{{ config('const.EATLOGS.NAME_LIST.Shoptel') }}</th>
-                                <th class="item-s">{{ config('const.EDIT') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($eatlogs as $item)
-                                <td>
-                                    <div><a href="{{ action('DetailController@show', $item->id) }}">{{ $item->shopname }}</a></div>
-                                    <div><a href="{{ $item->eatlogurl }}" target="_blank"><button type="button" class="eatlog-btn">{{ config('const.EATLOG') }}</button></a></div>
-                                </td>
-                                <td>{{ $item->score }}</td>
-                                <td><a href="tel:{{ $item->reserve_tel }}">{{ $item->reserve_tel }}</a></td>
-                                <td><a href="https://maps.google.co.jp/maps/search/{{ $item->address }}" target="_blank">{{ $item->address }}</a></td>
-                                <td>{{ $item->business_hours }}</td>
-                                <td><a href="tel:{{ $item->shoptel }}">{{ $item->shoptel }}</a></td>
-                                <td><a href="{{ action('EditController@edit', $item->id) }}"><button type="button" class="edit-btn">{{ config('const.EDIT') }}</button></a></td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="h3">全{{ count($eatlogs) }}件</div>
+      @foreach($eatlogs as $item)
+      <div class="card mt-3">
+        <div class="card-body">
+          <div><a href="{{ action('DetailController@show', $item->id) }}">
+              <h4>{{ $item->shopname }}</h4>
+            </a></div>
+          <div><a href="tel:{{ $item->reserve_tel }}">{{ $item->reserve_tel }}</a></div>
+          <div><a href="https://maps.google.co.jp/maps/search/{{ $item->address }}" target="_blank">{{ $item->address }}</a></div>
+          <div class="float-left"><a href="{{ $item->eatlogurl }}" target="_blank"><button type="button" class="list-button">{{ config('const.EATLOG') }}</button></a></div>
+          <div><a href="{{ action('EditController@edit', $item->id) }}"><button type="button" class="list-button">{{ config('const.EDIT') }}</button></a></div>
         </div>
+      </div>
+      @endforeach
     </div>
+  </div>
 </div>
 @endsection
